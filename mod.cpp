15afeeb7e774d5495669060b35d9e4baf1536cd9
Filip Maciejewski <veteran29q@gmail.com>
2019-08-15 16:10:09 +0200

    Fix logo path

--- mod.cpp
+++ mod.cpp
@@ -4,4 +4,4 @@ author = "ArmaForces";
 url = "https://github.com/armaforces";
 action = "https://github.com/armaforces";
 actionName = "GitHub";
-logo = "\armaforces_rhs_stryker\bft_fix\data\logo_256_ca.paa";
+logo = "\armaforces_rhs_stryker\bft_fix\data\logo_512_ca.paa";
